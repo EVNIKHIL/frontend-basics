052d9a986127d8b34c83fc090f09d77cd8c3afbf
Nikhil EV <evnikhilpba@gmail.com>
2020-06-12 10:54:32 +0530

    Second commit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  logoUrl: string = 'assets/reachout-logo.png';
   constructor() { }
 
   ngOnInit(): void {
